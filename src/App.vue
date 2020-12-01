f6f911007a58b93ff4f3e60f569a3db301def808
gleb <glebmikloshevich@gmail.com>
2020-12-01 22:35:04 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <p>{{getClicks}}</p>
+    <button @click="addOne">+1</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    getClicks(){
+      return this.$store.getters.getClicks
+    }
+  },
+  methods: {
+    addOne(){
+      this.$store.dispatch('updateNumber');
+    }
   }
 }
 </script>
@@ -22,7 +29,25 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+    color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  width: 100px;
+  height: 30px;
+  border: 2px solid #2c3e50;
+  border-radius: 8px;
+}
+
+p{
+  width: 140px;
+  height: 40px;
+  margin: 10px auto;
+  line-height: 45px;
+  border: 2px solid #2c3e50;
+  background-color: ghostwhite;
+  border-radius: 8px;
+  text-align: center;
+  vertical-align: middle;
 }
 </style>
